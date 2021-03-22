84083af56b97e6944887ceebe54d8c4b0672d4c4
copytorex@outlook.com <Engineer8$>
2021-03-21 20:20:29 -0500

    added an integration test

--- src/test/java/com/copytorex/restservices/RestservicesApplicationTests.java
+++ src/test/java/com/copytorex/restservices/RestservicesApplicationTests.java
@@ -1,13 +1,35 @@
 package com.copytorex.restservices;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.ResultMatcher.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RestservicesApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void createUser() throws Exception {
+		String json = "{\"name\": \"Kin\", \"birthDate\": \"2000-05-25T03:31:26.528+00:00\"}";
+		mockMvc.perform(
+				post("/users")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json)
+		).andExpect(
+				matchAll(
+						status().isCreated(),
+						content().json("{\"User ID\":1}")
+				)
+		);
 	}
 
 }
